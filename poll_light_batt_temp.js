34380b6ffd99fcc7dd3501a9e23af9182ad776a3
Burr Sutter <bsutter@redhat.com>
2015-05-02 20:23:31 -0400

    tweaking logic, sending to Edison

--- poll_light_batt_temp.js
+++ poll_light_batt_temp.js
@@ -5,12 +5,36 @@ var Bean = require('ble-bean');
 Bean.discover(function(bean){
   console.log('discovered: ', bean);
   if(bean.uuid ==="78a50449b65c") {
-	  bean.connectAndSetup(function() {
-		
-		
-	  }); // bean.connectAndSetup	
+      console.log("Found light, batt, temp bean");
+    
+      var pollForData = function() {
+    
+        bean.connectAndSetup(function() {
+            console.log("connectAndSetup");
+            
+            bean.notifyOne(
+            //called when there is data
+            console.log("notifyOne");
+            function(data){
+              if(data && data.length>=2){
+                var value = data[1]<<8 || (data[0]);
+                console.log("data: " + data);
+              }
+            },
+            //called when the notify is successfully or unsuccessfully setup
+            function(error){
+              if(error) console.log("one setup: ", error);
+            }); // bean.notifyOne(
+            
+        }); // bean.connectAndSetup 
+    
+      }; // pollForData
+    
+    // poll for sensor data every 10 secs
+    intervalId = setInterval(pollForData,10000);
+    
   } else {
    console.log("Not Light, Batt, Temp Bean");
-   process.exit(0);   	
+   process.exit(0);     
   }
 });
