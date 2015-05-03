7a5a030f60be6f097224f8ff031b71d6d806cd3c
Burr Sutter <bsutter@redhat.com>
2015-05-02 20:38:37 -0400

    tweaking logic, sending to Edison

--- poll_light_batt_temp.js
+++ poll_light_batt_temp.js
@@ -28,6 +28,24 @@ Bean.discover(function(bean){
             function(error){
               if(error) console.log("one setup: ", error);
             }); // bean.notifyOne(
+
+            bean.notifyTwo(
+            //called when there is data
+            function(data){
+            console.log("notifyOne");
+              if(data && data.length>=2){
+                console.log("length: " + data.length);
+	            var value0 = data[0];
+                console.log("value0: " + value0);
+                var value1 = data[1]<<8;
+                console.log("value1: " + value1);
+              }
+            },
+            //called when the notify is successfully or unsuccessfully setup
+            function(error){
+              if(error) console.log("one setup: ", error);
+            }); // bean.notifyTwo(
+
             
         }); // bean.connectAndSetup 
     
