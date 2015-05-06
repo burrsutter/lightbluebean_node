a361a334f757dc894cee00bf78372b14be1047ab
Burr Sutter <bsutter@redhat.com>
2015-05-06 10:06:34 -0400

    moving to edison

--- poller_temp.js
+++ poller_temp.js
@@ -4,29 +4,27 @@ var client = mqtt.connect('mqtt://192.168.3.2');
 var connectedBean;
 var intervalId;
 
-// question, can I discover, connectAndSetup, grab the temp and disconnect
-
-var pollForData = function() {
   Bean.discover(function(bean){
     // console.log('bean: ', bean); 
-    connectedBean = bean;    
-    console.log('uuid: ', connectedBean.uuid);
-    console.log('name: ', connectedBean._peripheral.advertisement.localName);
+    connectedBean = bean;
+    process.stdout.write("uuid: ", connectedBean.uuid);
+    process.stdout.write("\tname:", connectedBean._peripheral.advertisement.localName);
 
     connectedBean.on("temp", function(temp, valid){
       var status = valid ? "valid" : "invalid";
-      console.log(" temp:\t" + temp);
+      process.stdout.write("\ttemp:\t" + temp);
     }); // on temp
 
     connectedBean.connectAndSetup(function() {
-      console.log("connectedBean: connectAndSetup");
-      bean.requestTemp(     function(){
-        console.log("request temp sent");
-      });
+      // console.log("connectedBean: connectAndSetup");
+      var pollForData = function() {
+        bean.requestTemp(     function(){
+          // console.log("request temp sent");
+        });
+      } // pollForData
+      intervalId = setInterval(pollForData,5000);
 
     }); // connectAndSetup
   }); // discover
-} // pollForData
-// awaiting for next polling interval?
-intervalId = setInterval(pollForData,5000);
+
 
