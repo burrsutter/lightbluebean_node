5c1f938fcf1f4a6475ec450ca48ce710538aba11
Burr Sutter <bsutter@redhat.com>
2015-05-06 10:10:11 -0400

    moving to edison

--- poller_temp.js
+++ poller_temp.js
@@ -1,18 +1,18 @@
 var Bean = require('ble-bean');
 var mqtt = require('mqtt');
 var client = mqtt.connect('mqtt://192.168.3.2');
-var connectedBean;
+// var connectedBean;
 var intervalId;
 
   Bean.discover(function(bean){
     // console.log('bean: ', bean); 
     connectedBean = bean;
-    process.stdout.write("uuid: " + connectedBean.uuid);
-    process.stdout.write("\tname: " + connectedBean._peripheral.advertisement.localName);
 
     connectedBean.on("temp", function(temp, valid){
-      var status = valid ? "valid" : "invalid";
-      process.stdout.write("\ttemp:" + temp);
+      // var status = valid ? "valid" : "invalid";
+      process.stdout.write("uuid: " + connectedBean.uuid);
+      process.stdout.write("\tname: " + connectedBean._peripheral.advertisement.localName);
+      process.stdout.write("\ttemp:" + temp + "\n");
     }); // on temp
 
     connectedBean.connectAndSetup(function() {
