32a39852858d100a88930408469ac135d46400a6
Burr Sutter <bsutter@redhat.com>
2015-05-06 10:01:02 -0400

    moving to edison

--- poller.js
+++ poller.js
@@ -10,8 +10,10 @@ var pollForData = function() {
     console.log('uuid: ', bean.uuid);
     console.log('name: ', bean._peripheral.advertisement.localName);
     // console.log('bean: ', bean);
+    
   });
 }
-
+// question, can I discover, connectAndSetup, grab the temp and disconnect
+// awaiting for next polling interval?
 intervalId = setInterval(pollForData,1000);
 
---
+++ poller_temp.js
@@ -0,0 +1,32 @@
+var Bean = require('ble-bean');
+var mqtt = require('mqtt');
+var client = mqtt.connect('mqtt://192.168.3.2');
+var connectedBean;
+var intervalId;
+
+// question, can I discover, connectAndSetup, grab the temp and disconnect
+
+var pollForData = function() {
+  Bean.discover(function(bean){
+    // console.log('bean: ', bean); 
+    connectedBean = bean;    
+    console.log('uuid: ', connectedBean.uuid);
+    console.log('name: ', connectedBean._peripheral.advertisement.localName);
+
+    connectedBean.on("temp", function(temp, valid){
+      var status = valid ? "valid" : "invalid";
+      console.log(" temp:\t" + temp);
+    }); // on temp
+
+    connectedBean.connectAndSetup(function() {
+      console.log("connectedBean: ", );
+      bean.requestTemp(     function(){
+        console.log("request temp sent");
+      });
+
+    }); // connectAndSetup
+  }); // discover
+} // pollForData
+// awaiting for next polling interval?
+intervalId = setInterval(pollForData,5000);
+
