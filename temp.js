8e5f66198e8124189f5bf23d6e0f2f1b688a6eb8
Burr Sutter <bsutter@redhat.com>
2015-05-06 20:34:50 -0700

    moving to edison

---
+++ temp.js
@@ -0,0 +1,38 @@
+// https://www.npmjs.com/package/ble-bean
+
+var Bean = require('ble-bean');
+Bean.discover(function(bean){
+  console.log('discovered: ', bean);
+
+  bean.on("temp", function(temp, valid){
+    var status = valid ? "valid" : "invalid";
+    console.log("received " + status + " temp:\t" + temp);
+    console.log("\ttemperature = %d C", temp.toFixed(1));
+  });
+/*
+  bean.on("accell", function(x, y, z, valid){
+    var status = valid ? "valid" : "invalid";
+    console.log("received " + status + " accell\tx:\t" + x + "\ty:\t" + y + "\tz:\t" + z );
+  });
+*/
+
+  bean.connectAndSetup(function() {
+    console.log('connected');
+
+    var readData = function() {
+/*
+      bean.requestAccell(
+      function(){
+        console.log("request accell sent");
+      });
+*/
+      bean.requestTemp(
+      function(){
+        console.log("request temp sent");
+      });
+    }
+    // poll for sensor data every 1000
+    intervalId = setInterval(readData,1000);
+  });
+});
+
